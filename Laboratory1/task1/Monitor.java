1160605cba8e2068b390361d5f88b7b7ecfde8d1
Iulia-T <72933695+Iulia-T@users.noreply.github.com>
2021-10-11 02:27:49 +0300

    Delete Laboratory1 directory

--- Laboratory1/task1/Monitor.java
+++
@@ -1,8 +0,0 @@
-package lab1.task1;
-
-public class Monitor {
-    String name;
-    String color;
-    int dimension;
-    String resolution;
-}
--- Laboratory1/task2/Student.java
+++
@@ -1,7 +0,0 @@
-package lab1.task2;
-
-public class Student {
-    String name;
-    int age;
-    double mark;
-}
